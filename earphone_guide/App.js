fbd17195716abc22e3244a36faedad05cf82ec39
ZhangMingZhao1 <1104272319@qq.com>
2018-11-22 10:49:16 +0800

    done

--- earphone_guide/App.js
+++ earphone_guide/App.js
@@ -8,8 +8,9 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View, Image} from 'react-native';
-// import ripplePic from './public/img/ripple.png';
-
+let ripplePic = require('./public/img/ripple.png');
+let shapeBack = require('./public/img/shapeBack.png');
+let musicLogo = require('./public/img/musicLogo.png')
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android:
@@ -27,20 +28,29 @@ export default class App extends Component<Props> {
           <Text style={styles.span_mid}>双击左耳耳机</Text>
           <Text style={styles.span_bottom}>唤醒语音助手</Text>
           <View style={styles.ripple}>
-              <Image source={require('./public/img/ripple.png')} style={styles.rippleImg}/>  
+              <Image source={ripplePic} style={styles.rippleImg}/>  
               <View class="voice">
                 <Image source={require('./public/img/voice.png')} style={styles.voiceImg}/>  
               </View>
           </View>
           <Text style={styles.span_bar}>听到“嘀”声后，再说出以下话术</Text>
-          <View style={styles.show1}>
-              {/* <View style={style.box1}> */}
-                <Image source={require('./public/img/shapeBack.png')} style={styles.shapeBack}/>
-              {/* </View> */}
-              
-              {/* <Text>“打电话给张三”</Text> */}
-         
-          </View>        
+          <View style={styles.center}>
+              <View style={styles.box1}>
+                <Image source={shapeBack} style={styles.shapeBack}/>
+              </View>
+              <Image source={musicLogo} style={styles.musicLogo} style={styles.logo}/>
+              <Text style={styles.word}>“打电话给张三”</Text>
+          </View>    
+          <View style={styles.center}>
+              <View style={styles.box1}>
+                <Image source={shapeBack} style={styles.shapeBack}/>
+              </View>
+              <Image source={musicLogo} style={styles.musicLogo} style={styles.logo}/>
+              <Text style={styles.word}>“打电话给张三”</Text>
+          </View>   
+          <View roundAsCircle={true} style={styles.btnBottom}>
+            <Text style={styles.btnWord}>开始使用</Text>
+          </View>
       </View>
     );
   }
@@ -92,7 +102,7 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     color: '#FFFFFF',
   },
-  show1 :{
+  center :{
     alignItems: 'center',
  
   },
@@ -101,5 +111,39 @@ const styles = StyleSheet.create({
     width: 263.65,
     height: 75,
     marginTop: 17
+  },
+  logo : {
+    position: 'absolute',
+    width: 40,
+    height:40,
+    left: 80,
+    top: 35,
+  },
+  word: {
+    position: 'absolute',
+    fontSize:20,
+    color: '#FFFFFF',
+    top: 45,
+    left: 130
+    // marginLeft: 10,
+  },
+  btnBottom : {
+    position: 'absolute',
+    left:10,
+    bottom: 30,
+    width: 355,
+    height: 40,
+    backgroundColor: '#637EF8',
+    fontSize: 15,
+    lineHeight: 40,
+    color: '#FFFFFF',
+    textAlign: 'center',
+    borderRadius: 8,
+  },
+  btnWord: {
+    textAlign: 'center',
+    color: '#FFFFFF',
+    fontSize: 20,
+    marginTop:8,
   }
 });
